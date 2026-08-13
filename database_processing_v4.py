33867ea2b75fca555576a1afe1229a68b7b15c24
Yamil <essusyamil@gmail.com>
2026-08-13 13:06:14 -0400

    fix from interface
    Signed-off-by: Yamil <essusyamil@gmail.com>

--- database_processing_v4.py
+++ database_processing_v4.py
@@ -313,17 +313,17 @@ def process_single_day_period(database: str, hours: list):
 
         # Get a running proportion sum of DSD
         df["run_sum"] = df["dsd"].cumsum() / df["dsd"].sum()
-        # Get the smallest DSD above thresh to zero out actual table
+        # Get the largest DSD whose cumulative proportion is still below the threshold
         thresh_dsd = df["dsd"].loc[df["run_sum"] < utils.config["dsd_threshold"]].max()
+        thresh_dsd -= 1e-10  # Small buffer to avoid floating point issues
 
         # There might be nothing under the threshold if using few rep days
-        if not pd.isna(thresh_dsd):
-            thresh_dsd += 1e-12  # Small buffer to avoid floating point issues
+        if not pd.isna(thresh_dsd) and thresh_dsd < df["dsd"].max():
 
             # Set to zero where the proportion exceeds the threshold
             # If there are duplicate dsd values on the threshold these are all left in
             # This leaves everything in in the case of a flatline demand
-            df["dsd"] = df["dsd"].where(df["dsd"] >= thresh_dsd, 0)
+            df['dsd'] = df['dsd'].where(df['dsd'] >= thresh_dsd, 0)
 
             curs.execute(
                 f"""UPDATE demand_specific_distribution
